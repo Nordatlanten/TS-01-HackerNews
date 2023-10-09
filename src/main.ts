e312a300c5ff9916ebcbf5a08254c5db85c55d51
Atlan <gevaliatlaniskt@gmail.com>
2023-10-09 10:20:47 -0400

    alpha version ready

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,18 @@
 import './style.scss'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+
+import { setupNewsForm } from './news-form.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+    <h1>Welcome to Nordatlanten's portal for Hacker News</h1>
+    <form id="news-form"></form>
+    <div id="search-results"></div>
+    <div id="pagination">
+      <div id="navigation">
+      <button id="previous-button">Previous page</button><button id="next-button">Next Page</button>
+      </div>
+      <div id="page-counter"></div>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
   </div>
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setupNewsForm(document.querySelector<HTMLFormElement>('#news-form')!)
---
+++ src/news-form.ts
@@ -0,0 +1,226 @@
+import './style.scss'
+import axios from "axios"
+
+type ResultType = {
+  data: DataType
+}
+
+type DataType = {
+  hits: HitType[],
+  nbPages: number
+}
+
+type HitType = {
+  title: string,
+  url: string,
+  author: string,
+  points: number,
+  story_text: string,
+  comment_text: string,
+  _highlightResult: {
+    title: 
+    {
+        value: string,
+        matchLevel: string,
+        matchedWords: string[]
+    },
+    url: 
+    {
+        value: string,
+        matchLevel: string,
+        matchedWords: string[]
+    },
+    author: 
+    {
+        value: string,
+        matchLevel: string,
+        matchedWords: string[]
+    }
+  },
+  num_comments: number,
+  objectID: string,
+  _tags: string[],
+  page: number,
+  nbHits: number,
+  nbPages: number,
+  hitsPerPage: number,
+  processingTimeMS: number,
+  query: string,
+  params: string
+  created_at: string,
+  created_at_i: number
+}
+
+export function setupNewsForm(element: HTMLFormElement) {
+  const pagination = document.getElementById("pagination")
+  const nextButton = document.getElementById("next-button")
+  const previousButton = document.getElementById("previous-button")
+  const pageCounter = document.getElementById("page-counter")
+  let pageNumber = 1
+  let amountOfPages: number
+
+  //This function truncates long strings (only used for URLs right now)
+  const truncate = (source: string, size: number) : string => {
+    if (source) return source.length > size ? source.slice(0, size - 1) + "…" : source;
+    else return "No URL found."
+  }
+
+  //This method will do things when history state is changed
+  addEventListener("popstate", (_e) => {
+    grabURLParameters()
+    renderResults(history.state.query, history.state.pageNumber.toString())
+    if(pageCounter) pageCounter.innerHTML = `Page: ${history.state.pageNumber}`
+  });
+
+  const regexSearch = /(\?query=)([a-z]*)/g
+  const regexPage = /(&pageNumber=)[0-9]{1,2}/g
+  let currentURL = window.location.href
+  let foundSearch
+  let foundPage
+  
+  let searchParameter: string[] | undefined
+  let pageParameter: string[] | undefined
+
+
+  const grabURLParameters = async () => {
+  //grabs the query form the url
+    currentURL = window.location.href
+    foundSearch = currentURL.match(regexSearch)
+    foundPage = currentURL.match(regexPage)
+    if (foundSearch) searchParameter = foundSearch[0].split('=')
+    if (foundPage) pageParameter = foundPage[0].split('=')
+  }
+
+  //Grab the parameters on reload
+  grabURLParameters()
+
+  const renderResults = async (query: string, page: string) => {
+    let response = await axios.get(`http://hn.algolia.com/api/v1/search?query=${query}&page=${page}`).then((res) => res).catch(err => console.error(err)) as ResultType
+
+    amountOfPages = response.data.nbPages
+
+    if (element.nextElementSibling) element.nextElementSibling.innerHTML = ''
+    response.data.hits.forEach((item, _index) => {
+      let author = 'Author: ' + item.author
+      let title = item.title
+      let points = 'Points: ' + item.points
+      let link = item.url
+      let createdAt = 'Created at: ' + item.created_at
+
+      //create card elements
+      let newsCard = document.createElement('div')
+      let newsCardTopSection = document.createElement('div')
+      let newsCardBottomSection = document.createElement('div')
+      let newsTitle = document.createElement('h2')
+      let newsDateStamp = document.createElement('p')
+      let newsURL = document.createElement('a')
+      let pointsDisplay = document.createElement('p')
+      let authorDisplay = document.createElement('p')
+
+      //add card element attributes
+      newsCard.classList.add('card')
+      newsCardTopSection.classList.add('card__top-section')
+      newsCardBottomSection.classList.add('card__bottom-section')
+      newsTitle.classList.add('card__title')
+      newsDateStamp.classList.add('card__date-stamp')
+      newsURL.classList.add('card__news-url')
+      pointsDisplay.classList.add('card__points')
+      authorDisplay.classList.add('card__author')
+
+      //bind data to elements
+      newsTitle.innerText = title
+      newsDateStamp.innerText = createdAt
+      newsURL.innerText = truncate(link, 60)
+      newsURL.href = link
+      pointsDisplay.innerText = points.toString()
+      authorDisplay.innerText = author
+
+      //append children to parent
+      newsCardTopSection.appendChild(newsTitle)
+      newsCardTopSection.appendChild(newsURL)
+      newsCardBottomSection.appendChild(authorDisplay)
+      newsCardBottomSection.appendChild(pointsDisplay)
+      newsCardBottomSection.appendChild(newsDateStamp)
+      newsCard.appendChild(newsCardTopSection)
+      newsCard.appendChild(newsCardBottomSection)
+      element.nextElementSibling?.appendChild(newsCard)
+      
+      if (pagination) {
+        pagination.style.visibility= 'visible'
+      }
+      if(nextButton && pageNumber === amountOfPages - 1) {
+        nextButton.style.visibility = 'hidden'
+      } else if (nextButton && pageNumber < amountOfPages) {
+        nextButton.style.visibility = 'visible'
+      }
+      if (previousButton && pageNumber > 1) {
+        previousButton.style.visibility = 'visible'
+      } else if (previousButton && pageNumber <= 1){
+        previousButton.style.visibility = 'hidden'
+      }
+  }
+  )}
+
+  const fetchResults = async (query: string, page: string) => {
+    try {
+      await renderResults(query, page)
+      if(pageCounter) pageCounter.innerHTML = `Page: ${pageNumber}`
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  grabURLParameters()
+  //render stuff if parameters exist on reload
+  if (searchParameter && pageParameter) {
+    pageNumber = parseInt(pageParameter[1])
+    history.pushState({query: searchParameter[1], pageNumber: parseInt(pageParameter[1])}, `Searching: ${searchParameter[1]} - Page: ${pageParameter[1]}`, `?query=${searchParameter[1]}&pageNumber=${pageParameter[1]}`)
+    grabURLParameters()
+    fetchResults(searchParameter[1], pageParameter[1])
+  }
+
+  nextButton?.addEventListener("click", async (_e) => {
+    pageNumber++
+    history.pushState({query: history.state.query, pageNumber: pageNumber}, `Searching: ${history.state.query} - Page: ${pageNumber}`, `?query=${history.state.query}&pageNumber=${pageNumber}`)
+    grabURLParameters()
+    if (searchParameter && pageParameter) await fetchResults(searchParameter[1], pageParameter[1].toString())
+
+  })
+
+  previousButton?.addEventListener("click", async (_e) => {
+    pageNumber-- 
+    history.pushState({query: history.state.query, pageNumber: pageNumber}, `Searching: ${history.state.query} - Page: ${pageNumber}`, `?query=${history.state.query}&pageNumber=${pageNumber}`)
+    grabURLParameters() 
+    if (searchParameter && pageParameter) await fetchResults(searchParameter[1], pageParameter[1].toString())
+  })
+
+  const getNewsData = (e: SubmitEvent) =>  {
+    e.preventDefault()
+    pageNumber = 1
+    if (e.target) {
+      let query
+      let page
+      const data = new FormData(e.target as HTMLFormElement)
+      const dataObject = Object.fromEntries(data.entries());
+      query = dataObject.query
+      page = pageNumber
+      history.pushState({query: dataObject.query, pageNumber: pageNumber}, `Searching: ${dataObject.query} - Page: ${pageNumber}`, `?query=${dataObject.query}&pageNumber=${pageNumber}`)
+      grabURLParameters()  
+      fetchResults(query as string, page.toString())
+    }
+  }
+
+
+  //create form elements
+  const searchElement = document.createElement('input')
+  const submitElement = document.createElement('button')
+  searchElement.setAttribute("name", "query")
+  searchElement.setAttribute("type", "search")
+  searchElement.setAttribute("placeholder", "search news here...")
+  submitElement.setAttribute("type", "submit")
+  submitElement.innerText = "Search"
+  element.appendChild(searchElement)
+  element.appendChild(submitElement)
+
+  element.addEventListener('submit', (e) => getNewsData(e))
+}
